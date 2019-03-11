dccfc83faa8cc8954ad9ffd4ea75bb3707118d47
unknown <16tsmith55@gmail.com>
2019-03-10 23:23:27 -0600

    nearly done, still doesnt quite work

--- src/components/App.js
+++ src/components/App.js
@@ -1,107 +1,121 @@
 import React, { Component } from "react";
 import "../styles/App.scss";
 
-class App extends Component {
-  componentDidMount() {
-    this.setup();
-    this.state = { size: 20 };
-  }
-  setup() {
-    const color = "black";
-    const inverse = "white";
-    let grid = document.getElementById("grid");
-    var rows;
-    var cell;
-    let nextGrid = Array(this.state.size)
-      .fill(0)
-      .map(function() {
-        return Array(this.state.size).fill(0);
-      });
-    let interval;
 
-    function countNeighbors(grid, y, x) {
-      let sum = 0;
-      for (var rowIndex = -1; rowIndex <= 1; rowIndex++) {
-        for (var cellIndex = -1; cellIndex <= 1; cellIndex++) {
-          if (
-            grid.rows[y + rowIndex].cells[x + cellIndex].style
-              .backgroundColor == color
-          ) {
-            sum++;
-          }
+const judge = (game, newGame, size) => {
+  let neighbors = 0;
+
+  for (var i = 1; i < size-1; i++) {
+    for (var j = 1; j < size-1; j++) {
+      neighbors = countNeighbors(game, i, j);
+      if (
+        game[i][j] == true
+      ) {
+        if (neighbors < 2 || neighbors >= 4) {
+          newGame[i][j] = false;
         }
-      }
-      if (grid.rows[y].cells[x].style.backgroundColor == color) {
-        sum--;
-      }
-      return sum;
-    }
-    function buildNextGrid() {
-      let neighbors = 0;
-      for (var rowIndex = 1; rowIndex < this.state.size; rowIndex++) {
-        for (var cellIndex = 1; cellIndex < this.state.size; cellIndex++) {
-          neighbors = countNeighbors(grid, rowIndex, cellIndex);
-          if (
-            grid.rows[rowIndex].cells[cellIndex].style.backgroundColor == color
-          ) {
-            if (neighbors < 2 || neighbors >= 4) {
-              nextGrid[rowIndex][cellIndex] = 0;
-            }
-            if (neighbors == 2 || neighbors == 3) {
-              nextGrid[rowIndex][cellIndex] = 1;
-            }
-          } else {
-            if (neighbors == 3) {
-              nextGrid[rowIndex][cellIndex] = 1;
-            }
-          }
+        if (neighbors == 2 || neighbors == 3) {
+          newGame[i][j] = true;
+        }
+      } else {
+        if (neighbors == 3) {
+          newGame[i][j] = true;
         }
       }
     }
-    function updateGrid() {
-      for (var rowIndex = 1; rowIndex < this.state.size; rowIndex++) {
-        for (var cellIndex = 1; cellIndex < this.state.size; cellIndex++) {
-          if (nextGrid[rowIndex][cellIndex] == 1) {
-            grid.rows[rowIndex].cells[cellIndex].style.backgroundColor = color;
-          } else {
-            grid.rows[rowIndex].cells[
-              cellIndex
-            ].style.backgroundColor = inverse;
-          }
-        }
-      }
-    }
-    function createTable() {
-      rows = [];
-      for (var rowIndex = 0; rowIndex < this.state.size; rowIndex++) {
-        cell = [];
-        for (var cellIndex = 0; cellIndex < this.state.size; cellIndex++) {
-          cell.push(<td />);
-        }
-        rows.push(<tr>{cell}</tr>);
-      }
-    }
-    function run() {
-      createTable();
-      //preset live squares
-      for (var i = 6; i < 16; i++) {
-        grid.rows[5].cells[i].style.backgroundColor = color;
-      }
-      grid.rows[2].cells[1].style.backgroundColor = color;
-      grid.rows[2].cells[2].style.backgroundColor = color;
-      grid.rows[2].cells[3].style.backgroundColor = color;
-      //set automated life-cycle
-      interval = window.setInterval(function() {
-        buildNextGrid();
-        updateGrid();
-      }, 500);
-    }
-    run(); // create table, darken cells, start interval
   }
+}
+let countNeighbors = (game, yPos, xPos) => {
+  let sum = 0;
+  for (var i = -1; i <= 1; i++) {
+    for (var j = -1; j <= 1; j++) {
+        game[yPos + i][xPos + j] == false ? sum ++ : sum--;
+    }
+  }
+  if (game[yPos][xPos] == true) {
+    sum--;
+  }
+  return sum;
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const size = 20;
+    const game = [];
+
+    // create empty game
+    for (let i = 0; i < size; i++) {
+      const row = [];
+      for (let j = 0; j < size; j++) {
+        row.push(false);
+      }
+      game.push(row);
+    }
+
+    // inital state for testing
+    // for (var i = 0; i < 10; i++) {
+    //   game[5][5+i] = true;
+    // }
+
+    game[5][5] = true;
+    game[5][6] = true;
+    game[5][7] = true;
+
+    this.state = {
+      game, size
+    };
+
+    // use bind to force correct 'this' which has this.state
+    this.tick = this.tick.bind(this);
+    //this.size = this.size.bind(this);
+  }
+
+  componentDidMount() {
+    window.setInterval(this.tick, 1000);
+  }
+
+  tick() {
+    const { game, size} = this.state; // current game state
+    const newGame = [];
+
+    for (let i = 0; i < size; i++) {
+      const row = [];
+      for (let j = 0; j < size; j++) {
+        row.push(false);
+      }
+      newGame.push(row);
+    }
+
+
+    // look at each cell in current game state and calculate if needs to die or live...
+    judge(game, newGame, size);
+    // update newGame with next game state
+
+
+
+    // game logic...
+
+    this.setState({
+      game: newGame
+    })
+  }
+
   render() {
+    // const game = this.state.game;
+    const { game } = this.state;
     return (
-      <table id="grid">
-        <tbody>{rows}</tbody>
+      <table>
+        <tbody>
+          {game.map(row => (
+            <tr>
+              {row.map(cell => (
+                <td className={cell ? 'living' : ''}></td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
       </table>
     );
   }
